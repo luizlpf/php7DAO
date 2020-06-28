9669778664c67d81854c98747fd6d6a7909d25a2
Luiz Paulo Freire <luizpaulolpf@yahoo.com.br>
2020-06-28 16:27:17 -0300

    criado a classe usuario

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("config.php");
+/*
 
 $sql = new Sql();
 
@@ -8,5 +8,12 @@ $sql = new Sql();
 $usuarios = $sql->select("SELECT*FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
 
+require_once("config.php");
+$root = new Usuario();
+
+$root->loadById(7);
+
+echo $root;
 ?>
