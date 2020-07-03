1fc5fbcc68db3867368464b307d9961e759d3cf7
Luiz Paulo Freire <luizpaulolpf@yahoo.com.br>
2020-07-03 12:36:24 -0300

    Iniciado o projeto

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
 
-spl_autoload_register(function($class_name){
+	spl_autoload_register(function($class_name){
 
-	$filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";
+		$filename = "classe".DIRECTORY_SEPARATOR.$class_name.".php";
 
-	if (file_exists(($filename))) {
-		require_once($filename);
-	}
-});
+		if(file_exists(($filename))){
+
+			require_once($filename);
+		}
+
+	});
 
 ?>
--- index.php
+++ index.php
@@ -1,45 +1,11 @@
 <?php
 
-/*
+require_once("config.php");
 
 $sql = new Sql();
 
-
-$usuarios = $sql->select("SELECT*FROM tb_usuarios");
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
-*/
 
-require_once("config.php");
-
-
-//carrega um usuario
-
-/*$root = new Usuario();
-
-$root->loadById(7);
-
-echo $root;
-*/
-
-//carrega uma lista de usuario;
-/*$lista = Usuario::getList();
-
-echo json_encode($lista);
-*/
-
-//carrega uma lista de usuario buscando pelo login
-
-/*$search = Usuario::search("ul");
-
-echo json_encode($search);
-*/
-
-
-//carrega um usuario pelo login e senha
-$usuario = new Usuario();
-
-$usuario->login("Luiz", "45678912");
-
-echo $usuario;
 ?>
